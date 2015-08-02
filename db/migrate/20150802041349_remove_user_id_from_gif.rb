cf15bcb2fa1c9567cc6347ed88902cee3ee693ef
Raquel Moss <raquelxmoss@gmail.com>
2015-08-03 08:45:14 +1200

    Remove unneccessary column from Gif schema

--- db/migrate/20150802041349_remove_user_id_from_gif.rb
+++ db/migrate/20150802041349_remove_user_id_from_gif.rb
@@ -3,3 +3,13 @@ class RemoveUserIdFromGif < ActiveRecord::Migration
     remove_column :gifs, :user_id
   end
 end
+
+
+details = details.to_a
+
+details.each_with_index do |hash, index|
+  meal_ingredients.each do |measurement|
+    details[index][1]["quantity"] = meal_ingredients[index].quantity
+    details[index][1]["measurement"] = meal_ingredients[index].measurement
+  end
+end

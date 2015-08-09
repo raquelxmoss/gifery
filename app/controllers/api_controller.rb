8c21bfc3617f60c369bc868f4f323fa3cd916937
Raquel Moss <raquelxmoss@gmail.com>
2015-08-09 17:14:35 +1200

    When saving a Gif via the API, a UserGif is created rather than just a Gif

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ActionController::Base
 
   def save_gif
     # http://localhost:3000/api/v1/save-gif?url=http://editorial.designtaxi.com/editorial-images/news-lovesleep290715/2-love-sleep.jpg&user_id=1
-    if @gif = Gif.create(url: params[:url], user_id: params[:user_id])
+    if @gif = UserGif.create(gif: Gif.find_or_create_by(url: params[:url]), user: User.find(params[:user_id]))
       render json: @gif
     else
       render json:

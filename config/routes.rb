213674531ca5ed42d8d041d55ac84e8e1d31e617
Raquel Moss <raquelxmoss@gmail.com>
2015-08-11 12:33:49 +1200

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
-  # resources :tags
-  # resources :gifs
   root 'application#index'
   devise_for :users
 
   namespace :api do
-    resources :gifs, only: [:create, :show, :new, :destroy]
-    resources :user_gifs, only: [:create, :show, :new, :destroy]
+    resources :user_gifs, only: [:create, :show, :destroy]
   end
-
-  # get 'api/v1/save-gif', to: 'api#save_gif', as: 'save_gif'
-  # get 'api/v1/tag-gif', to: 'api#tag_gif', as: 'tag_gif'
-  # delete 'api/v1/delete-gif', to: 'api#delete_gif', as: 'delete_gif'
-  # delete 'api/v1/delete-tag', to: 'api#delete_tag', as: 'delete_tag'
 
 end

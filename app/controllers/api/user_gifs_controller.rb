f7d8f0e0a1acc645b3e451cd4c02d947641dee1e
Raquel Moss <raquelxmoss@gmail.com>
2015-08-11 12:19:31 +1200

    Users can view, create, and destroy gifs

--- app/controllers/api/user_gifs_controller.rb
+++ app/controllers/api/user_gifs_controller.rb
@@ -4,10 +4,7 @@ class Api::UserGifsController < ApplicationController
     if @gif = UserGif.create(gif: Gif.find_or_create_by(url: params[:url]), user: User.find(params[:user_id]))
       render json: @gif
     else
-      render json:
-      {
-        error: "Sorry, something went wrong with your request. Please check the API documentation and try again."
-      }
+      generic_error
     end
   end
 
@@ -15,9 +12,27 @@ class Api::UserGifsController < ApplicationController
   end
 
   def destroy
+    if gif = UserGif.find_by(id: params[:id])
+      gif.destroy
+      render json: {
+        message: "Gif deleted"
+      }
+    else
+      generic_error
+    end
   end
 
   def show
+    @gif = UserGif.find_by(id: params[:id])
+    render json: @gif
+  end
+
+  private
+
+  def generic_error
+    render json: {
+      error: "Sorry, something went wrong with your request. Please check the API documentation and try again"
+    }
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def index
-
+    @gifs = Gif.all
   end
 
 end

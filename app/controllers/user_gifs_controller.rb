3a5f465ead4a91ba39e27da4f53903e478676946
Raquel Moss <raquelxmoss@gmail.com>
2015-08-13 16:13:57 +1200

    Add UserGifsController and routes

---
+++ app/controllers/user_gifs_controller.rb
@@ -0,0 +1,3 @@
+class UserGifsController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'application#index'
   devise_for :users
+  resources :user_gifs
+  resources :gifs, only: [:index, :show]
 
   namespace :api do
     resources :user_gifs, only: [:create, :show, :destroy]

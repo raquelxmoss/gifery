300ce4dc91aacb2d73564c99c172d8e37c69afa1
Raquel Moss <raquelxmoss@gmail.com>
2015-08-13 16:23:26 +1200

    Move methods to more sensible places

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,41 +1,11 @@
 class GifsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :show]
-
   def index
     @gifs = Gif.all
   end
 
   def show
     @gif = Gif.find(params[:id])
-  end
-
-# TODO: Refactor the below to suit the new data structure. New and Create are no longer needed, and these methods need to be moved to the UserGifsController
-  def new
-    @gif = Gif.new
-    @tag = Tag.new
-  end
-
-  def create
-    if @gif = UserGif.create!(user: current_user, gif: Gif.find_or_create_by(gif_params))
-      create_tags(@gif.gif)
-      redirect_to gifs_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def gif_params
-    params.require(:gif).permit(:url)
-  end
-
-  def create_tags(gif)
-    tag_string = params[:gif][:tags][:name]
-    tag_string.split(",").each do |tag|
-      gif.tags.create(name: tag.strip)
-    end
   end
 
 end
--- app/controllers/user_gifs_controller.rb
+++ app/controllers/user_gifs_controller.rb
@@ -1,3 +1,62 @@
 class UserGifsController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :load_user_and_gif, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @gifs = UserGif.where(user: current_user)
+  end
+
+  def show
+  end
+
+  def new
+    @gif = UserGif.new(user: current_user)
+  end
+
+  def create
+    if @gif = UserGif.create(user: current_user, gif: Gif.find_or_create_by(url: user_gif_params[:url]))
+      render :show, id: @gif.id
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @gif.update(user_gif_params)
+      render :show, id: @gif.id
+    else
+      render :edit, id: @gif.id
+    end
+  end
+
+  def destroy
+    if @gif.destroy
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def user_gif_params
+    # params.require(:user_gif).permit(:url) ???
+  end
+
+  def load_user_and_gif
+    @gif = UserGif.find(params[:id]).gif
+    @user = current_user
+  end
+
+  # def create_tags(gif)
+  #   tag_string = params[:gif][:tags][:name]
+  #   tag_string.split(",").each do |tag|
+  #     gif.tags.create(name: tag.strip)
+  #   end
+  # end
+
 end

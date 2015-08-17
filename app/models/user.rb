f82cc37423dda889dac84e6ecdef8ca0359f424f
Raquel Moss <raquelxmoss@gmail.com>
2015-08-17 17:16:40 +1200

    Install devise_token_auth gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+
   root 'application#index'
   devise_for :users
+
   resources :users, only: [:show, :index] do
     resources :user_gifs
   end
+
   resources :gifs, only: [:index, :show]
 
   namespace :api do

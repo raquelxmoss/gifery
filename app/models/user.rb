3b55d2f015a7a87e9b56381bb18c7309dc12ca6e
Raquel Moss <raquelxmoss@gmail.com>
2015-08-17 17:21:59 +1200

    Add routes for API authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
 
   root 'application#index'
   devise_for :users
@@ -12,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :user_gifs, only: [:create, :show, :destroy]
+    mount_devise_token_auth_for 'User', at: 'auth'
   end
 
 end

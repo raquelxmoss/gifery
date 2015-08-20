49fcf3ddddf2e5b86cc5c619be9982a0b6b9078c
Raquel Moss <raquelxmoss@gmail.com>
2015-08-21 07:56:27 +1200

    Change to has_and_belongs_to_many

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   root 'application#index'
   devise_for :users
 
-  resources :users, only: [:show, :index] do
-    resources :user_gifs
-  end
-
+  resources :users, only: [:show, :index]
   resources :gifs, only: [:index, :show]
 
   namespace :api do
-    resources :user_gifs, only: [:create, :show, :destroy]
+    resources :gifs, only: [:create, :show, :destroy]
     mount_devise_token_auth_for 'User', at: 'auth'
   end
 

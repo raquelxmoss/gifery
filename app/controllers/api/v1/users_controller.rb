99e32dd508ab77744ab32f9a44be562ed6fe284d
Raquel Moss <raquelxmoss@gmail.com>
2015-08-25 20:49:48 +1200

    Add api gifs and sessions controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
+  before_action :authenticate_with_token!, only: [:update, :destroy]
   respond_to :json
 
   def show
@@ -16,7 +17,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
+    user = current_user
 
     if user.update(user_params)
       render json: user, status: 200, location: [:api, user]
@@ -26,8 +27,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
+    current_user.destroy
     head 204
   end
 

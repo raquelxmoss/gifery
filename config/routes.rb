fff6651733bf0ae31d143385ffd7be4b161bbc4f
Raquel Moss <raquelxmoss@gmail.com>
2015-08-25 17:25:57 +1200

    Remove api constraints cause they were fucking with shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
-require 'api_constraints'
 Rails.application.routes.draw do
   root 'application#index'
-  devise_for :users
 
-   namespace :api, defaults: { format: :json },
-                                constraints: { subdomain: 'api' }, path: '/'  do
-      scope module: :v1,
-                constraints: ApiConstraints.new(version: 1, default: true) do
+   namespace :api, defaults: { format: :json }, path: '/'  do
+      scope module: :v1 do
         resources :users, only: [:show]
       end
     end
+  # devise_for :users
 end
 

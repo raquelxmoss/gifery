c8ffa9f1fac14eb5e9a0177816f9d606144bcf4b
Raquel Moss <raquelxmoss@gmail.com>
2015-08-25 17:29:50 +1200

    Add user update endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
    namespace :api, defaults: { format: :json }, path: '/'  do
       scope module: :v1 do
-        resources :users, only: [:show, :create]
+        resources :users, only: [:show, :create, :update]
       end
     end
   # devise_for :users

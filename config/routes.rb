3d7aaf5c7bca02f0bbb4463fdd677d62da2f15e6
Raquel Moss <raquelxmoss@gmail.com>
2015-08-25 16:57:19 +1200

    Something something something versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
+require 'api_constraints'
 Rails.application.routes.draw do
   root 'application#index'
   devise_for :users
 
-  namespace :api, defaults: { format: :json } do
-    # http://api.localhost:3000/v1/products/1
-    constraints: { subdomain: 'api' }, path: '/'  do
-       scope module: :v1 do
-          # We are going to list our resources here
-        end
-     end
-   end
-
-
+   namespace :api, defaults: { format: :json },
+                                constraints: { subdomain: 'api' }, path: '/'  do
+      scope module: :v1,
+                constraints: ApiConstraints.new(version: 1, default: true) do
+        # We are going to list our resources here
+      end
+    end
 end
+

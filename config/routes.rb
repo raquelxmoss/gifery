cfb3bd275b72a8869ecc2c4b9d27f2f4a3127546
Raquel Moss <raquelxmoss@gmail.com>
2015-08-25 16:40:39 +1200

    Set up container for nicely versioned api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   root 'application#index'
   devise_for :users
+
+  namespace :api, defaults: { format: :json } do
+    # http://api.localhost:3000/v1/products/1
+    constraints: { subdomain: 'api' }, path: '/'  do
+       scope module: :v1 do
+          # We are going to list our resources here
+        end
+     end
+   end
+
+
 end
